803245dedba87a396bd220d5fc0834e5a12461a8
IMS212 <ims@ims21.tech>
2022-02-27 13:48:29 -0800

    lerp, make inject

--- src/main/java/io/github/retinamc/retina/mixin/modules/damagetilt/BobHurtMixin.java
+++ src/main/java/io/github/retinamc/retina/mixin/modules/damagetilt/BobHurtMixin.java
@@ -7,43 +7,23 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameRenderer.class)
 public class BobHurtMixin {
+	@Shadow
+	@Final
 	private Minecraft minecraft;
 
 	/**
 	 * @author Akashii_Kun
 	 */
-	@Overwrite
-	private void bobHurt(PoseStack matrixStack, float partialTicks) {
-		if (this.minecraft.getCameraEntity() instanceof LivingEntity) {
-			LivingEntity livingEntity = (LivingEntity)this.minecraft.getCameraEntity();
-
-			float f = livingEntity.hurtTime - partialTicks;
-
-			if (livingEntity.isDeadOrDying()) {
-				float f1 = Math.min(livingEntity.deathTime + partialTicks, 20.0F);
-
-				matrixStack.mulPose(Vector3f.ZP.rotationDegrees(40.0F - 8000.0F / (f1 + 200.0F)));
-			}
-
-			if (f < 0.0F) {
-				return;
-			}
-			f /= livingEntity.hurtDuration;
-			f = Mth.sin(f * f * f * f * 3.1415927F);
-			float g = livingEntity.hurtDir;
-
-			if(livingEntity instanceof Player player)
-			g =	(float)(Mth.atan2(player.zo - player.getZ(), player.xo - player.getX()) * (180D / Math.PI) - (double)player.getYRot());
-
-			matrixStack.mulPose(Vector3f.YP.rotationDegrees(-g));
-			matrixStack.mulPose(Vector3f.ZP.rotationDegrees(-f * 14.0F));
-			matrixStack.mulPose(Vector3f.YP.rotationDegrees(g));
-		}
+	@Redirect(method = "bobHurt", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/LivingEntity;hurtDir:F"))
+	private float editHurtDirection(LivingEntity entity) {
+		return (float) (Mth.atan2(Mth.lerp(Minecraft.getInstance().getFrameTime(), entity.zo - entity.getZ(), entity.getZ()), Mth.lerp(Minecraft.getInstance().getFrameTime(), entity.xo - entity.getX(), entity.getX())) * (180D / Math.PI) - (double)entity.getYRot());
 	}
 }
