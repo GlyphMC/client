5470b2531c117999d04ebceaff14bea2fc9f9a44
NoComment1105 <nocomment1105@outlook.com>
2022-04-28 19:38:29 +0100

    Quiltify

--- src/main/java/io/github/arctanmc/arctan/ArctanClient.java
+++ src/main/java/io/github/arctanmc/arctan/ArctanClient.java
@@ -1,16 +1,17 @@
 package io.github.arctanmc.arctan;
 
 import io.github.arctanmc.arctan.rpc.DiscordRPC;
-import net.fabricmc.api.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class ArctanClient implements ClientModInitializer {
 	public static final String MOD_ID = "arctan";
 	public static final Logger LOGGER = LogManager.getLogger("ArctanClient");
 
 	@Override
-	public void onInitializeClient() {
+	public void onInitializeClient(ModContainer mod) {
 		LOGGER.info("Initialized");
 		DiscordRPC.init();
 	}
