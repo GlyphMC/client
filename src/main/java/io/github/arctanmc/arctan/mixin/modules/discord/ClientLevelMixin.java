b220022da450e4a9a75b9187e0a6957373bf4cca
chalkyjeans <aaryan.verma@hotmail.com>
2022-08-06 06:54:46 +0100

    Properly distinguish between singleplayer and multiplayer

--- src/main/java/io/github/arctanmc/arctan/mixin/modules/discord/ClientLevelMixin.java
+++ src/main/java/io/github/arctanmc/arctan/mixin/modules/discord/ClientLevelMixin.java
@@ -2,9 +2,12 @@ package io.github.arctanmc.arctan.mixin.modules.discord;
 
 import io.github.arctanmc.arctan.rpc.RPCEvents;
 import io.github.arctanmc.arctan.rpc.RPCUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.AbstractClientPlayer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,16 +15,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientLevel.class)
 public class ClientLevelMixin {
 
+	@Shadow
+	@Final
+	private Minecraft minecraft;
+
 	@Inject(method = "addPlayer", at = @At("HEAD"))
 	public void addPlayer(int id, AbstractClientPlayer player, CallbackInfo ci) {
-		System.out.println("client level add player");
-		RPCEvents.getInstance().gameRPC(RPCUtils.GameType.SINGLEPLAYER);
+		if (minecraft.isLocalServer()) {
+			RPCEvents.getInstance().gameRPC(RPCUtils.GameType.SINGLEPLAYER);
+		} else {
+			RPCEvents.getInstance().gameRPC(RPCUtils.GameType.MULTIPLAYER);
+		}
 	}
-
-	/*
-	TODO Distinguish better between singleplayer and multiplayer:
-	Singleplayer: first server level add player is triggered, then client level add player
-	Multiplayer: only client level add player is triggered
-	 */
 
 }
