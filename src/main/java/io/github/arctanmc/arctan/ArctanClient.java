79ae537e9f19a659aec78f690ef319fa06e6472f
NoComment1105 <nocomment1105@outlook.com>
2022-08-06 09:26:23 +0100

    Fix commit

--- src/main/java/io/github/arctanmc/arctan/ArctanClient.java
+++ src/main/java/io/github/arctanmc/arctan/ArctanClient.java
@@ -2,6 +2,7 @@ package io.github.arctanmc.arctan;
 
 import io.github.arctanmc.arctan.rpc.DiscordRPC;
 import net.minecraft.ChatFormatting;
+import okhttp3.OkHttpClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.quiltmc.loader.api.ModContainer;
@@ -13,8 +14,10 @@ import java.io.IOException;
 public class ArctanClient implements ClientModInitializer {
 
 	public static final String MOD_ID = "arctan";
+	public static final Long CLIENT_ID = 947514926589693983L;
 	private static Version VERSION;
 	public static final Logger LOGGER = LogManager.getLogger("ArctanClient");
+	public static final OkHttpClient HTTP_CLIENT = new OkHttpClient();
 
 	@Override
 	public void onInitializeClient(ModContainer mod) {
