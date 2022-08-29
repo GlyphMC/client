ec277fd5ef260c21036a0faa4e10213df91b164a
chalkyjeans <aaryan.verma@hotmail.com>
2022-08-29 19:20:54 +0100

    Give discord settings it's own screen

--- src/main/java/io/github/arctanmc/arctan/gui/SettingsScreen.java
+++ src/main/java/io/github/arctanmc/arctan/gui/SettingsScreen.java
@@ -7,54 +7,27 @@ import dev.lambdaurora.spruceui.option.SpruceOption;
 import dev.lambdaurora.spruceui.option.SpruceSimpleActionOption;
 import dev.lambdaurora.spruceui.screen.SpruceScreen;
 import dev.lambdaurora.spruceui.widget.SpruceButtonWidget;
-import dev.lambdaurora.spruceui.widget.SpruceLabelWidget;
-import io.github.arctanmc.arctan.util.API;
-import io.github.arctanmc.arctan.util.User;
-import net.minecraft.Util;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import okhttp3.HttpUrl;
 
 import java.awt.*;
 
 public class SettingsScreen extends SpruceScreen {
 
 	private final Screen parent;
-
-	private final SpruceOption linkDiscord;
-	private final String uuid = Minecraft.getInstance().getUser().getUuid().replace("-", "");
+	private final SpruceOption discord;
 
 	public SettingsScreen(Screen parent) {
-		super(Component.literal("Arctan Client Settings"));
+		super(Component.translatable("arctan.client.settings.title"));
 		this.parent = parent;
-		this.linkDiscord = SpruceSimpleActionOption.of("arctan.client.settings.discord", button -> {
-			HttpUrl url = new HttpUrl.Builder()
-					.scheme("https")
-					.host("discord.com")
-					.addPathSegment("oauth2")
-					.addPathSegment("authorize")
-					.addQueryParameter("response_type", "code")
-					.addQueryParameter("client_id", "947514926589693983")
-					.addQueryParameter("scope", "identify")
-					.addQueryParameter("state", uuid)
-					.addQueryParameter("prompt", "consent")
-					.addQueryParameter("redirect_uri", "http://localhost:8080/verify") // TODO CHANGE THIS
-					.build();
-			Util.getPlatform().openUri(String.valueOf(url));
-		}, Component.literal("Link to Discord"));
+		this.discord = SpruceSimpleActionOption.of("arctan.client.settings.discord", button -> this.minecraft.setScreen(new DiscordSettingsScreen(this)));
 	}
 
 	@Override
 	protected void init() {
 		super.init();
 		int buttonHeight = 20;
-		User user = API.getUserByUuid(uuid);
-		if (user != null) { // TODO ADD GUI ELEMENTS
-			this.addRenderableWidget(new SpruceLabelWidget(Position.of(this, this.width / 2 - 150, this.height / 2 + buttonHeight), Component.literal(user.tag()), 300));
-		} else {
-			this.addRenderableWidget(this.linkDiscord.createWidget(Position.of(this, this.width / 2 - 75, this.height / 2 - buttonHeight), 150));
-		}
+		this.addRenderableWidget(this.discord.createWidget(Position.of(this, this.width / 2 - 75, this.height / 2 - buttonHeight), 150));
 		this.addRenderableWidget(new SpruceButtonWidget(Position.of(this, this.width / 2 - 75, this.height - 29), 150, buttonHeight, SpruceTexts.GUI_DONE, button -> {
 			this.minecraft.setScreen(this.parent);
 		}));
