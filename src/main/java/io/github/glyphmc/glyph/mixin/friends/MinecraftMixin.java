41aa3dbd5d2cd6a7f1a978f50c7e2689617085b7
chalkyjeans <73715948+chalkyjeans@users.noreply.github.com>
2023-07-16 10:28:09 +0100

    WIP Friends System

---
+++ src/main/java/io/github/glyphmc/glyph/mixin/friends/MinecraftMixin.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 GlyphMC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.glyphmc.glyph.mixin.friends;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static io.github.glyphmc.glyph.GlyphClient.OKHTTP;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+
+	@Inject(method = "destroy", at = @At("HEAD"))
+	private void glyph_destroy(CallbackInfo ci) {
+		OKHTTP.dispatcher().executorService().shutdown();
+	}
+
+}
